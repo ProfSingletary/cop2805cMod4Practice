a3f8beeddd5fef2d77ea4a5b218b7cff65d5d85f
ProfSingletary <david.singletary@fscj.edu>
2022-06-01 15:48:59 -0400

    new version, fixed Xlint warnings

--- CountVowelsAndConsonants.java
+++ CountVowelsAndConsonants.java
@@ -1,39 +1,53 @@
-import java.util.*;
+// CountVowelsAndConsonants.java
+// Uses HashSets to count and display vowels and consonants
+// in response to an input string from the user.
+// Assume A, E, I, O, and U are the vowels. The same vowels
+// or consonants are counted only once.
+// The letters are case-insensitive.
 
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+// counts unique vowels and consonants in a String
 public class CountVowelsAndConsonants {
-    public static void main(String args[]) {
-        System.out.print("Please enter a string: ");
+    public static void main(String[] args) {
+        System.out.print("Enter a string: ");
         Scanner input = new Scanner(System.in);
         String s = input.nextLine();
-        s = s.toUpperCase();
-        
-        Character[] list1 = {'A','E','I','O','U'};
-        
+       
+        Character[] list1 = {'A', 'E', 'I', 'O', 'U'};
+       
+        // store our vowels and consonants in HashSets
         Set<Character> vowels = new HashSet<>(Arrays.asList(list1));
         Set<Character> vowelsInString = new HashSet<>();
         Set<Character> consonantsInString = new HashSet<>();
-        
-        int vowelCount = 0;
-        int consonantCount = 0;
-        
-        for (int index = 0; index < s.length(); index++) {            
-            char letter = s.charAt(index);
-            
-            if (vowels.contains(letter)) {
-                vowelsInString.add(letter);
-            }
-            else if (letter == ' ') {
-                continue;
-            }
-            else {
-                consonantsInString.add(letter);
-            }
+       
+        int numberOfVowels = 0;
+        int numberOfConsonants = 0;
+       
+        for (int i = 0; i < s.length(); i++) {
+              char ch = Character.toUpperCase(s.charAt(i));
+             
+              // if current character is not a letter, don't do anything   
+              if (Character.isLetter(ch)) {
+                    if (vowels.contains(ch)) {
+                         // found a vowel, see if we've already counted it
+                         if (!vowelsInString.contains(ch)) {
+                            vowelsInString.add(ch);
+                            numberOfVowels++;
+                         }
+                    }
+                    // else we have a consonant, see if it is already counted
+                    else if (!consonantsInString.contains(ch)) {
+                        consonantsInString.add(ch);
+                        numberOfConsonants++;
+                    }
+              }
         }
-        
-        System.out.println(vowelsInString);
-        System.out.println(consonantsInString);
-        
-        System.out.println("Number of vowels: " + vowelsInString.size());
-        System.out.println("Number of consonants: " + consonantsInString.size());
+    
+        System.out.println("The number of vowels is " + numberOfVowels);
+        System.out.println("The number of consonants is " + numberOfConsonants);
     }
 }
